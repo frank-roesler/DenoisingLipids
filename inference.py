2cc7338c7ecaa295ca8a788e1441503b6f44ea42
Frank Roesler <frank.roesler@posteo.de>
2024-10-20 11:47:22 +0200

    update inference.py

--- inference.py
+++ inference.py
@@ -1,3 +1,5 @@
+import platform
+
 import scipy
 import numpy as np
 import torch.cuda
@@ -6,28 +8,36 @@ from nets import DiffusionNet, UNet
 from utils_infer import denoise_signal
 import matplotlib.pyplot as plt
 from scipy import io
+from pathlib import Path
+import pathlib
+myPlatform = platform.system()
+if myPlatform == 'Darwin':
+    pathlib.WindowsPath = pathlib.PosixPath
+print(myPlatform)
 
 NoiseFit     = True # Set to "True" if model was trained to fit the noise, "False" if trained to fit signal
 DiffusionFit = True # Set to "True" if a 2d model was used to fit all b-values simultaneously
 
-model_path = 'trained_models/DiffusionNet_compr_15x3_16x3_32'
-matPath    = 'result.mat'
+model_path = 'trained_models/DiffusionNet_compr_15x3_16x3_32/model.pth'
+matPath    = 'data/__DATEN/mrsiData_Lip.mat'
 
 device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 print(device)
-checkpoint = torch.load(model_path, map_location=device)
-
-model = checkpoint['model']
+model = torch.load(model_path, map_location=device, weights_only=False)
 model.eval()
-print(model)
 
 mat    = scipy.io.loadmat(matPath)
-mrsSet = np.array(mat['metabData'])
-y = fftshift(ifft( np.conj( mrsSet ) ), 1 )
+mrsSet = np.array(mat['mrsiData'][0][0][1][:,492:498])
 
+print(mrsSet)
+
+y = ifft( np.conj( mrsSet ), axis=0)
+y = fftshift(y, axes=0 )
+plt.plot(np.real(y), linewidth=0.5)
+plt.show()
 # Denoise signal:
 y_dn_cplx = denoise_signal(y, model, diffusion=DiffusionFit, noise_fit=NoiseFit, device=device)
-
-
-plt.plot(np.real(y_dn_cplx[:,1500:2500].T), linewidth=0.5)
+#
+#
+plt.plot(np.real(y_dn_cplx), linewidth=0.5)
 plt.show()
--- utils_infer.py
+++ utils_infer.py
@@ -5,15 +5,14 @@ import numpy as np
 def complex_to_tensor(signal, normalize=True, diffusion=False):
     if normalize:
         if diffusion:
-            maxValues = np.max(np.abs(signal[0, :]))
+            maxValues = np.max(np.abs(signal), axis=0)
         else:
             maxValues = np.array(np.max(np.abs(signal), 1))[np.newaxis]
-        print(signal)
         signal = signal / maxValues.T
     if diffusion:
         y_tensor = torch.from_numpy(signal.squeeze()).cfloat()
         y_tensor = torch.stack([torch.real(y_tensor), torch.imag(y_tensor)]).unsqueeze(0)
-        y_tensor = np.transpose(y_tensor, (0,1,3,2)) # (1, 2, 4096, 6)
+        # y_tensor = np.transpose(y_tensor, (0,1,3,2)) # (1, 2, 4096, 6)
     else:
         y_tensor = torch.from_numpy(signal.squeeze()).cfloat()
         y_tensor = torch.stack([torch.real(y_tensor), torch.imag(y_tensor)])
@@ -26,9 +25,8 @@ def complex_to_tensor(signal, normalize=True, diffusion=False):
 def denoise_signal(signal, model, diffusion=False, device=torch.device("cpu"), noise_fit=False):
     """applies trained model to noisy spectra"""
     with torch.no_grad():
-        model = model
         y_tensor, maxValues = complex_to_tensor(signal, diffusion=diffusion)
-        y_tensor = y_tensor
+
         if noise_fit:
             noise = model(y_tensor.to(device)).cpu()
             y_dn = y_tensor-noise
@@ -39,5 +37,5 @@ def denoise_signal(signal, model, diffusion=False, device=torch.device("cpu"), n
             y_dn = y_dn.squeeze()
             y_dn = np.transpose(y_dn, (2,0,1))
         y_dn_cplx = (y_dn[:, 0, :] + y_dn[:, 1, :]* 1j).squeeze()
-        y_dn_cplx = y_dn_cplx * maxValues.T
+        y_dn_cplx = y_dn_cplx.T * maxValues
     return y_dn_cplx
