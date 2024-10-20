4fafd3d5f11221574a0002420fa7ac07b0833507
Frank Roesler <frank.roesler@posteo.de>
2024-10-20 15:06:03 +0200

    noiseLvl grändert

--- config_train.py
+++ config_train.py
@@ -13,7 +13,7 @@ lr              = 6e-5
 batch_size      = 32    # will be multiplied by n_bvals
 plot_loss_every = 1000    # plot and print info every n epochs
 
-modeldir  = 'trained_models/' # save model as
+modeldir  = 'trained_models/metab_fit' # save model as
 modelname = modeldir + 'DiffusionNet_compr_15x3_16x3_32' # load this model
 
 bvals = range(32,32+1)
