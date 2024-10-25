345d8aa453ad7998ba65e78277e7f6cb6088ef8e
Frank Roesler <frank.roesler@posteo.de>
2024-10-25 08:21:26 +0200

    added torch.compile

--- src/train.py
+++ src/train.py
@@ -9,6 +9,7 @@ from src.configs.config_simul import *
 from src.configs.config_train import *
 import matplotlib.pyplot as plt
 
+
 metab_basis = Metab_basis(metab_path, kwargs_BS, metab_con, normalize_basis_sets=NormalizeBasisSets)
 mmbg_basis  = MMBG_basis(mmbg_path, kwargs_MM, reduce_small_mm=ReduceSmallMMs) if includeMMBG else None
 lip_basis   = Lip_basis(lip_path, kwargs_Lipd ) if includeLip else None
@@ -39,40 +40,48 @@ if LoadPretrainedModel:
 print('training...')
 info_screen = InfoScreen(output_every=plot_loss_every, plot_spectra_during_train=plotSpectraDuringTraining)
 model.train()
-while epoch <= epochs+1:
-    if timer>2000:
-        if batch_size==128:
-            break
-        batch_size *= 2
-        timer = 100
-    loss = torch.zeros(1, device=device)
-    for n_bvals in bvals:
-        noisy_signal_batch, noise_batch, lip_batch = make_batch_diffusion( batch_size, n_bvals, metab_basis, mmbg_basis, lip_basis,
-                                                                           restrict_range=None, #(1500,2500), 
-                                                                           #restrict_range=(0,404), 
-                                                                           include_mmbg = includeMMBG,
-                                                                           include_lip  = includeLip,
-                                                                           normalization='max_1', monotone_diffusion=Monotonicity,
-                                                                           **kwargs_BS)
-        noisy_signal_batch = noisy_signal_batch.to(device)
-        noise_batch        = noise_batch.to(device)
-        lip_batch          = lip_batch.to(device)
-        pred   = model(noisy_signal_batch)
-        target = noisy_signal_batch - lip_batch - noise_batch
-        loss  += loss_fn(pred, target)/len(bvals)
-        info_screen.plot_spectra(epoch, n_bvals, noisy_signal_batch, target, pred)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.01)
-    optimizer.step()
-    losses.append(loss.cpu().detach().data[0])
-    info_screen.print_info(losses, optimizer, epoch, epochs, model, noise_batch.shape[0]*len(bvals))
-    info_screen.plot_losses(epoch, losses)
 
-    current_loss = np.mean(losses[-window_for_current_loss:])
-    timer = checkpoint.save(timer, current_loss, epoch, model, optimizer, losses, best_loss)
-    timer += 1
-    epoch += 1
+@torch.compile
+def train(model, batch_size, epoch, epochs, timer):
+    while epoch <= epochs+1:
+        if timer>2000:
+            if batch_size==128:
+                break
+            batch_size *= 2
+            timer = 100
+        loss = torch.zeros(1, device=device)
+        for n_bvals in bvals:
+            noisy_signal_batch, noise_batch, lip_batch = make_batch_diffusion( batch_size, n_bvals, metab_basis, mmbg_basis, lip_basis,
+                                                                               restrict_range=None, #(1500,2500),
+                                                                               #restrict_range=(0,404),
+                                                                               include_mmbg = includeMMBG,
+                                                                               include_lip  = includeLip,
+                                                                               normalization='max_1', monotone_diffusion=Monotonicity,
+                                                                               **kwargs_BS)
+            noisy_signal_batch = noisy_signal_batch.to(device)
+            noise_batch        = noise_batch.to(device)
+            lip_batch          = lip_batch.to(device)
+            pred   = model(noisy_signal_batch)
+            target = noisy_signal_batch - lip_batch - noise_batch
+            loss  += loss_fn(pred, target)/len(bvals)
+            info_screen.plot_spectra(epoch, n_bvals, noisy_signal_batch, target, pred)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.01)
+        optimizer.step()
+        losses.append(loss.cpu().detach().data[0])
+        info_screen.print_info(losses, optimizer, epoch, epochs, model, noise_batch.shape[0]*len(bvals))
+        info_screen.plot_losses(epoch, losses)
+
+        current_loss = np.mean(losses[-window_for_current_loss:])
+        timer = checkpoint.save(timer, current_loss, epoch, model, optimizer, losses, best_loss)
+        timer += 1
+        epoch += 1
+    plt.show()
+
+train(model, batch_size, epoch, epochs, timer)
 
 
-plt.show()
+
+
+
